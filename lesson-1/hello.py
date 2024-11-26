6f79f4416e00b4d6fefde17d6e87fa808caf8eba
afdhali <afdhali.apreto@gmail.com>
2024-11-26 15:40:05 +0700

    adding information

--- lesson-1/hello.py
+++ lesson-1/hello.py
@@ -1,2 +1,20 @@
 greeting = 'Hello World'
 print(greeting)
+# ==========================
+# JS vs Python
+"""
+Python:
+
+Secara default berjalan synchronous
+Kode dieksekusi baris per baris
+Harus menunggu baris sebelumnya selesai
+Menggunakan async/await untuk asynchronous
+
+
+JavaScript:
+
+Secara default berjalan asynchronous
+Menggunakan callback, Promise, atau async/await
+Bisa menjalankan kode secara parallel
+Tidak harus menunggu kode sebelumnya selesai
+"""
